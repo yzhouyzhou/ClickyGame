f447da918b870cddc71d98e292e25ab943c91a1d
Ying Zhou <yzhouyzhou@yahoo.com>
2019-09-04 13:45:59 -0500

    Updates

---
+++ precache-manifest.a59a85a586b6774738e6d17b6d81c09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d05b8cef0771c48ea4f15f76afbebf",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "4b50587215dd088fad36",
+    "url": "/ClickyGame/static/css/main.82c9f2ba.chunk.css"
+  },
+  {
+    "revision": "c93f329e8ecac95cc6f6",
+    "url": "/ClickyGame/static/js/2.bd2e74d0.chunk.js"
+  },
+  {
+    "revision": "4b50587215dd088fad36",
+    "url": "/ClickyGame/static/js/main.36eef17c.chunk.js"
+  },
+  {
+    "revision": "3f94739a733620af8917",
+    "url": "/ClickyGame/static/js/runtime~main.1df70f2f.js"
+  },
+  {
+    "revision": "14737e21e1d07d87602f9c88ddb59e33",
+    "url": "/ClickyGame/static/media/math-background.14737e21.jpg"
+  }
+]);
